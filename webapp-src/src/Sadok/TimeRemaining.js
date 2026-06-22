ecd055b330a8f8ee684d32bf7c68dc1141a3d007
Nicolas Mora <github@babelouest.org>
2026-06-22 14:00:56 -0400

    Fix and improve time remaining

--- webapp-src/src/Sadok/TimeRemaining.js
+++ webapp-src/src/Sadok/TimeRemaining.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import i18next from 'i18next';
 
 export default function TimeRemaining({offset, textSpeed, tokens, compact}) {
   let timeRemainingMin = 0, timeRemainingSec = 0, timeRemainingHour = 0, remainingJsx, remainingTxt = "";
@@ -7,17 +8,27 @@ export default function TimeRemaining({offset, textSpeed, tokens, compact}) {
     timeRemainingHour = Math.floor(remaining/60);
     timeRemainingMin = Math.floor(remaining%60);
     timeRemainingSec = Math.floor((remaining%1)*60);
-    if (timeRemainingHour > 0) {
-      remainingTxt += (timeRemainingHour<10?"0":"") + timeRemainingHour + "h";
+    if (timeRemainingHour < 10) {
+      timeRemainingHour = "0" + timeRemainingHour;
     }
-    if (timeRemainingMin > 0 || timeRemainingHour > 0) {
-      remainingTxt += (timeRemainingMin<10?"0":"") + timeRemainingMin + "m";
+    if (timeRemainingMin < 10) {
+      timeRemainingMin = "0" + timeRemainingMin;
     }
-    remainingTxt += (timeRemainingSec<10?"0":"") + timeRemainingSec + "s";
+    if (timeRemainingSec < 10) {
+      timeRemainingSec = "0" + timeRemainingSec;
+    }
     if (compact) {
-      remainingJsx = remainingTxt;
+      return (
+      <>
+        {i18next.t("time-remaining-compact", {h: timeRemainingHour, m: timeRemainingMin, s: timeRemainingSec})}
+      </>
+      )
     } else {
-      remainingJsx = " - " + remainingTxt + " restant";
+      return (
+      <>
+        {i18next.t("time-remaining-long", {h: timeRemainingHour, m: timeRemainingMin, s: timeRemainingSec})}
+      </>
+      )
     }
   }
   return (
