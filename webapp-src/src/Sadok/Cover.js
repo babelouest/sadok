da08f48a8e3d46713c25ceda4ab0c3ae61dabb7a
Nicolas Mora <github@babelouest.org>
2026-06-22 14:01:24 -0400

    Fix show cover bug

--- webapp-src/src/Sadok/Cover.js
+++ webapp-src/src/Sadok/Cover.js
@@ -4,13 +4,13 @@ export default function Cover({book, opacity, showCover}) {
   const [ imgData, setImgData ] = useState(false);
   
   useEffect(() => {
-    if (showCover && book.book?.resources?.cover) {
-      book.book.loadBlob(book.book.resources.cover.href)
+    if (book && showCover && book.resources?.cover) {
+      book.loadBlob(book.resources.cover.href)
       .then(res => {
         const reader = new FileReader();
         reader.readAsDataURL(res);
         reader.onloadend = () => {
-          const dataUrlPrefix = `data:${book.book.resources.cover.mediaType};base64,`;
+          const dataUrlPrefix = `data:${book.resources.cover.mediaType};base64,`;
           setImgData(dataUrlPrefix+reader.result.split(",")[1]);
         };
       });
@@ -19,7 +19,7 @@ export default function Cover({book, opacity, showCover}) {
     }
   },[book,showCover]);
 
-  if (showCover && imgData) {
+  if (showCover && imgData && book.resources?.cover) {
     return (
       <div className="perfect-centering cover">
         <img src={imgData} className={"img-fluid opacity-"+opacity} />
