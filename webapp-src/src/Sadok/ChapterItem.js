831ee6746e3e7f70d65f21c1a78f0307b72ba55b
Nicolas Mora <github@babelouest.org>
2026-06-22 14:00:06 -0400

    Fix chapter item show

--- webapp-src/src/Sadok/ChapterItem.js
+++ webapp-src/src/Sadok/ChapterItem.js
@@ -14,31 +14,36 @@ export default function ChapterItem({config, totalTokens, chapter, chapterOffset
       <TimeRemaining offset={0} textSpeed={config.speedReaderTextSpeed} tokens={chapterOffset+chapter.tokens} compact={true}/>
       &nbsp;(<TimeRemaining offset={chapterOffset} textSpeed={config.speedReaderTextSpeed} tokens={totalTokens} compact={true}/>)
     </>
-  let percent = Math.round((chapterOffset/totalTokens)*100), className, currentId = "";
+  let percent = Math.round((chapterOffset/totalTokens)*100), className = "", currentId = "";
   if (offset >= chapterOffset && offset < (chapterOffset + chapter.tokens)) {
     className = " active";
     currentId = "sadok-current-chapter";
   }
   return (
-        <>
-          <a className={"list-group-item"+className}
-             href="#"
-             onClick={(e) => selectChapter(e, chapterOffset)}
-             aria-current="true"
-             id={currentId}
-             data-bs-toggle="offcanvas"
-             data-bs-target="#LeftMenu">
-            <div className="d-flex w-80 justify-content-between">
-              <h6 className="mb-1 fw-bold">
-                {chapter.label}
-              </h6>
-              <small>
-                {i18next.t("percent", {val: percent})+" - "+chapterOffset}
-                {/*remainingJsx*/}
-              </small>
-            </div>
-          </a>
-          <hr/>
-        </>
+    <>
+      <a className={"list-group-item"+className}
+         href="#"
+         onClick={(e) => selectChapter(e, chapterOffset)}
+         aria-current="true"
+         id={currentId}
+         data-bs-toggle="offcanvas"
+         data-bs-target="#LeftMenu">
+        <div className="d-flex w-80 justify-content-between">
+          <h6 className="mb-1 fw-bold">
+            {chapter.label}
+          </h6>
+          <div>
+            <small>
+              {i18next.t("percent", {val: percent})+" - "+chapterOffset}
+            </small>
+          </div>
+          {/*<div>
+            <small>
+              {remainingJsx}
+            </small>
+          </div>*/}
+        </div>
+      </a>
+    </>
   );
 }
