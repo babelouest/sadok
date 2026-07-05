1b5bff3094d81d0f478c41ff49ef7b237fcf20bb
Nicolas Mora <github@babelouest.org>
2026-07-05 14:24:12 -0400

    Update copyright year

--- profile-server/js/index.js
+++ profile-server/js/index.js
@@ -4,7 +4,7 @@
  *
  * Profile management API in javascript
  *
- * Copyright 2024 Nicolas Mora <mail@babelouest.org>
+ * Copyright 2026 Nicolas Mora <mail@babelouest.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU GENERAL PUBLIC LICENSE
